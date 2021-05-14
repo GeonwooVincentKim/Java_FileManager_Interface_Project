392b3e0f900b6682b67d3b69ac5c808e7e19c1c1
GeonwooVincentKim <kdsnop@gmail.com>
2021-05-14 16:35:51 +0900

    [Modify] - Trying to separates codes

--- src/SortProgram/SortMain.java
+++ src/SortProgram/SortMain.java
@@ -1,11 +1,9 @@
 package SortProgram;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.util.Scanner;
 
 import FileManager.AssistantFileManager;
-import FileManager.FileManager;
+// import SortManager.Sort;
 import TypeConverter.TypeConverter;
 
 public class SortMain {
@@ -16,6 +14,47 @@ public class SortMain {
     public static void writeFile(AssistantFileManager asFM, String result){
         asFM.writeFile("result1.txt", result);
     }
+
+    // public static int[] bubbleSort(String splitLines[]){
+    //     int []convertSplitLines = new int[splitLines.length];
+    //     int [] splitNumbers = convertSplitLines;
+
+    //     int temp = 0;
+    //     int i = 0, j = 0, k = 0;
+
+    //     for(i = 0; i < splitLines.length; i++){
+    //         convertSplitLines[i] = Integer.parseInt(splitLines[i]);
+    //     }
+
+    //     for(j = 0; j < splitNumbers.length; j++){
+    //         for (k = 0; k < (splitNumbers.length - k) - 1; k++){
+    //             if(splitNumbers[k] > splitNumbers[k + 1]){
+    //                 temp = splitNumbers[k];
+    //                 splitNumbers[k] = splitNumbers[k + 1];
+    //                 splitNumbers[k + 1] = temp;
+    //             }
+    //         }
+    //     }
+
+    //     return convertSplitLines;
+    // }
+
+    // public static int[] bubbleSort(TypeConverter tCon, String splitLines[]){
+    //     int []convertSplitLines = tCon.convertToInt(splitLines);
+    //     int temp = 0;
+
+    //     for(int i = 0; i < convertSplitLines.length; i++){
+    //         for (int j = 0; j < (convertSplitLines.length - j) - 1; j++){
+    //             if(convertSplitLines[j] > convertSplitLines[j + 1]){
+    //                 temp = convertSplitLines[j];
+    //                 convertSplitLines[j] = convertSplitLines[j + 1];
+    //                 convertSplitLines[j + 1] = temp;
+    //             }
+    //         }
+    //     }
+
+    //     return convertSplitLines;
+    // }
 
     public static String getData(TypeConverter tCon, Scanner readFile){
         int readLine = Integer.parseInt(readFile.nextLine());
@@ -36,7 +75,7 @@ public class SortMain {
 
             for (int j = 0; j < convertSplitLine.length; j++){
                 System.out.print(convertSplitLine[j] + " ");
-
+                
             }
             System.out.println();
         }
@@ -48,8 +87,8 @@ public class SortMain {
     public static void main(String[] args) {
         AssistantFileManager mFM = new AssistantFileManager();
         TypeConverter mTCon = new TypeConverter();
+        // Sort mSort = new Sort();
 
-        Scanner fileReader = readFile(mFM, "data1.txt");
         String getData = getData(mTCon, readFile(mFM, "data1.txt"));
         writeFile(mFM, getData);
     }
